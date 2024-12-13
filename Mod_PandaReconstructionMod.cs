b498162550a65524f030867acf7a5dad2c82eb77
jason0320 <jasonnosaj0320@gmail.com>
2024-12-13 20:46:28 +0800

    加入專案檔案。

---
+++ Mod_PandaReconstructionMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace PandaReconstructionMod
+{
+    [BepInPlugin("panda.recon.mod", "Panda's Reconstruction Mod", "1.0.0.0")]
+    internal class Mod_PandaReconstructionMod : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("Panda's Reconstruction Mod");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ ReconPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace PandaReconstructionMod
+{
+    [HarmonyPatch]
+    internal class ReconPatch
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(InvOwnerReconstruction), "ShouldShowGuide")]
+        static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
